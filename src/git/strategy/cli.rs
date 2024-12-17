5b50c4e7ed3657a81ea76781a8500a14bc6607e1
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-12-17 11:23:23 -0800

    Simplify/canonicalize path before passing to 'git' since it doesn't handle Windows UNC paths e.g., "\\?\E:\source". Adds dependency on 'dunce' for path simplification.

--- src/git/strategy/cli.rs
+++ src/git/strategy/cli.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path};
 use std::process::Command;
 
 use anyhow::anyhow;
@@ -9,6 +9,7 @@ use crate::git::{CheckoutBranch, CloneOptions, CloneRepository, Fetch};
 pub struct Cli;
 
 impl CloneRepository for Cli {
+
     fn clone_repository<U, P>(&self, url: U, path: P, options: &CloneOptions) -> anyhow::Result<()>
     where
         U: ToString,
@@ -16,10 +17,14 @@ impl CloneRepository for Cli {
     {
         debug!("Cloning the repository using CLI strategy");
 
+        // Convert `path` to `&Path` and pass it to `dunce::simplified`
+        let simplified_path = dunce::simplified(path.as_ref());
+        let path_local = simplified_path.to_string_lossy(); // Use lossless conversion to String
+
         let mut args = vec![
             "clone".to_string(),
             url.to_string(),
-            path.as_ref().to_string_lossy().to_string(),
+            path_local.into_owned(), // Convert Cow<str> into String
         ];
 
         if let Some(recursive) = options.recursive.as_ref() {
@@ -38,6 +43,7 @@ impl CloneRepository for Cli {
             args.push(format!("--branch={branch}"));
         }
 
+        debug!("Executing: git {}", args.join(" "));
         let output = Command::new("git").args(args).output()?;
         match output.status.success() {
             true => Ok(()),
